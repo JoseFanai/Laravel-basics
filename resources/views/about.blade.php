c373849a5f10f36549547328033aa0d1966f68d6
Your Name <josefanai0102@gmail.com>
2026-06-15 23:41:23 +0530

    updating git

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
 <body style="font-family: Arial, Helvetica, sans-serif; text-align:center; margin: top 50px;">
 
     <h1 style="color: #ff2d20;">Welcome to Laravel world!</h1>
-    <p>This is the first page created using Blade. It is not a normal text.</p>
+    <p>This is the about page created using Blade. It is not a normal text.</p>
     <button style="padding: 10px 20px; background-color:black; color: white; border-radius: 5px;">Click</button>
     
 </body>
